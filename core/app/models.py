f50d4707fc7511f3a3c07a90c441ae1152a51cc6
amianivictor <khahonjeri.amiani@student.moringaschool.com>
2022-05-02 11:26:37 +0300

    add class articles

--- core/app/models.py
+++ core/app/models.py
@@ -11,3 +11,21 @@ class Sources:
         self.category = category
         self.country = country
 
+
+class Articles:
+    """
+     '''
+    Article  class to define Article Objects
+    '''
+    """
+    def __init__(self,id,name,description,author,url,title,urlToImage,content,publishedAt):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.author = author
+        self.url = url
+        self.title = title
+        self.urlToImage=urlToImage
+        self.content= content
+        self.publishedAt= publishedAt
+       
